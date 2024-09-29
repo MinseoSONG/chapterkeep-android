066ac6a929040e8301dc78333fa3344c14f402cf
MinseoSong <mins041600@naver.com>
2024-09-29 23:36:20 +0900

    스플래시 화면 구현

--- app/src/main/java/com/chapter/chapterkeep/screen/startScreen/SplashScreen.kt
+++ app/src/main/java/com/chapter/chapterkeep/screen/startScreen/SplashScreen.kt
@@ -1,8 +1,56 @@
 package com.chapter.chapterkeep.screen.startScreen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.chapter.chapterkeep.R
 
 @Composable
 fun SplashScreen() {
-
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorResource(id = R.color.main_green))
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.img_chapterkeep_whitelogo),
+                contentDescription = "앱 로고",
+                modifier = Modifier.size(90.dp)
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(
+                text = stringResource(id = R.string.app_name),
+                color = colorResource(R.color.white),
+                fontWeight = FontWeight.Black,
+                fontSize = 30.sp
+            )
+            Text(
+                text = stringResource(R.string.app_sub_name),
+                color = colorResource(R.color.white),
+                textAlign = TextAlign.Center,
+                fontSize = 20.sp
+            )
+        }
+    }
 }
