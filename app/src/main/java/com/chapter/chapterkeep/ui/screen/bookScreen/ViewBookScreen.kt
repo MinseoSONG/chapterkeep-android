bf142d7cd8b69ef3720e2d4cb4530123236ddc9f
MinseoSong <mins041600@naver.com>
2024-11-06 14:20:07 +0900

    #10 배경 이미지 설정 완료

--- app/src/main/java/com/chapter/chapterkeep/ui/screen/bookScreen/ViewBookScreen.kt
+++ app/src/main/java/com/chapter/chapterkeep/ui/screen/bookScreen/ViewBookScreen.kt
@@ -1,14 +1,20 @@
 package com.chapter.chapterkeep.ui.screen.bookScreen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.chapter.chapterkeep.R
 import com.chapter.chapterkeep.ui.component.header.HeaderWhiteLogo
 
@@ -28,10 +34,22 @@ fun ViewBookScreen(
                 .padding(20.dp)
         ) {
             Box(
-
+                modifier = Modifier
+                    .fillMaxSize()
             ){
-
+                Image(
+                    painter = painterResource(R.drawable.img_viewbook_background),
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop
+                )
             }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewViewBookScreen() {
+    ViewBookScreen(navController = rememberNavController())
 }
