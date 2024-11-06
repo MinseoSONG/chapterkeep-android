428737a8646cb78957a64773bd3c42cef475337f
MinseoSong <mins041600@naver.com>
2024-11-06 14:54:09 +0900

    #10 책 소개바 구현

--- app/src/main/java/com/chapter/chapterkeep/ui/screen/bookScreen/ViewBookScreen.kt
+++ app/src/main/java/com/chapter/chapterkeep/ui/screen/bookScreen/ViewBookScreen.kt
@@ -3,10 +3,18 @@ package com.chapter.chapterkeep.ui.screen.bookScreen
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
@@ -17,11 +25,15 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.chapter.chapterkeep.R
 import com.chapter.chapterkeep.ui.component.header.HeaderWhiteLogo
+import com.chapter.chapterkeep.ui.screen.bookScreen.component.ViewBookBar
 
 @Composable
 fun ViewBookScreen(
     navController: NavHostController
 ) {
+    var bookTitle by remember{ mutableStateOf("책 제목") }
+    var bookWriter by remember{ mutableStateOf("글쓴이") }
+
     Scaffold(
         topBar = {
             HeaderWhiteLogo()
@@ -31,18 +43,39 @@ fun ViewBookScreen(
         Column(
             modifier = Modifier
                 .padding(paddingValues)
-                .padding(20.dp)
+                .padding(start = 20.dp, end = 20.dp, bottom = 20.dp)
         ) {
             Box(
                 modifier = Modifier
                     .fillMaxSize()
-            ){
+            ) {
                 Image(
                     painter = painterResource(R.drawable.img_viewbook_background),
                     contentDescription = null,
                     modifier = Modifier.fillMaxSize(),
                     contentScale = ContentScale.Crop
                 )
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 35.dp, horizontal = 30.dp)
+                ) {
+                    ViewBookBar(
+                        image = R.drawable.img_home_book,
+                        bookTitle = bookTitle,
+                        bookWriter = bookWriter
+                    )
+                    Spacer(Modifier.height(5.dp))
+
+                    Divider(
+                        modifier = Modifier.fillMaxWidth(),
+                        thickness = 1.dp,
+                        color = colorResource(R.color.gray_400)
+                    )
+
+
+                }
             }
         }
     }
---
+++ app/src/main/java/com/chapter/chapterkeep/ui/screen/bookScreen/component/ViewBookBar.kt
@@ -0,0 +1,62 @@
+package com.chapter.chapterkeep.ui.screen.bookScreen.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.chapter.chapterkeep.R
+
+@Composable
+fun ViewBookBar(
+    image: Int,
+    bookTitle: String,
+    bookWriter: String
+) {
+    Row(
+        modifier = Modifier
+            .height(100.dp)
+            .fillMaxWidth()
+    ) {
+        Spacer(modifier = Modifier.width(10.dp))
+        Image(
+            painter = painterResource(image),
+            contentDescription = null,
+            modifier= Modifier
+                .padding(end = 20.dp)
+                .size(62.dp, 100.dp)
+        )
+
+        Column(
+            modifier = Modifier.fillMaxHeight(),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = bookTitle,
+                fontSize = 15.sp,
+                fontWeight = FontWeight.SemiBold,
+                modifier = Modifier.padding(bottom = 5.dp)
+            )
+
+            Text(
+                text = bookWriter,
+                fontSize = 12.sp,
+                color = colorResource(R.color.gray_700)
+            )
+        }
+    }
+}
